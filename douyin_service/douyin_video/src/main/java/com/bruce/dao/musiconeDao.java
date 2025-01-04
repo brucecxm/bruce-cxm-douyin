311a7fa8d2407cf351d5a0fcfb19d5738aa79df7
Bruce chen <130543450+brucecxm@users.noreply.github.com>
2025-01-04 12:00:16 +0800

    区分测试环境和开发环境

---
+++ douyin_service/douyin_video/src/main/java/com/bruce/dao/musiconeDao.java
@@ -0,0 +1,19 @@
+package com.bruce.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface musiconeDao {
+
+    @Select("SELECT c.*,d.* FROM music c " +
+            "LEFT JOIN video d ON c.music_video_id = d.video_id " +
+            "WHERE c.music_id = #{music_id}")
+    List<Map> one(@Param("music_id") String music_id);
+
+
+}
