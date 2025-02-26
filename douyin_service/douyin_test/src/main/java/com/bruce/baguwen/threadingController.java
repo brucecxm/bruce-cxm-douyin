ec4c2f34cbfc00843d5d1c839c226dd46f0600ac
brucecxm <2945090920@qq.com>
2025-02-26 17:26:18 +0800

    增加了锁的八股文
    增加了锁的八股文

--- douyin_service/douyin_test/src/main/java/com/bruce/baguwen/threadingController.java
+++ douyin_service/douyin_test/src/main/java/com/bruce/baguwen/threadingController.java
@@ -12,6 +12,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import static java.lang.Thread.sleep;
+
 @RestController
 @RequestMapping("/threading")
 public class threadingController {
@@ -22,7 +24,7 @@ public class threadingController {
         ExecutorService executor= Executors.newFixedThreadPool(5);
         Future<?> stockFuture = executor.submit(() -> {
             try {
-                Thread.sleep(2000); // 模拟一个耗时操作
+                sleep(2000); // 模拟一个耗时操作
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -39,7 +41,7 @@ public class threadingController {
         * */
         Future<?> stockFutureone = executor.submit(() -> {
             try {
-                Thread.sleep(2000); // 模拟一个耗时操作
+                sleep(2000); // 模拟一个耗时操作
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -199,6 +201,178 @@ public class threadingController {
 
 
     }
+
+
+
+
+
+
+    private static int counter = 0;
+
+
+    /*
+    * 1. 实例同步方法（普通同步方法）：---------对象级别 就是类实例出来的级别
+锁定的是当前对象的实例：当使用 synchronized 修饰一个实例方法时，锁是作用在调用该方法的对象实例上。在 increment() 方法中，锁是对 SynchronizedExample 类的实例对象加锁的。也就是说，同一时刻只有一个线程能够执行该实例的 increment() 方法。
+影响同一对象的并发：多个线程可以并发执行不同对象的同步方法，但在同一个对象实例上，只有一个线程能执行同步方法，其他线程需要等待。
+    *
+    *
+    * 2. 静态同步方法： ------------类的级别  类没有实例  类本身的级别
+锁定的是类级别的 Class 对象：静态方法是属于类的，而不是某个具体的对象实例。
+* 当一个静态方法使用 synchronized 修饰时，锁定的是类的 Class 对象（即 StaticSynchronizedExample.class）。
+* 这意味着所有线程在执行 increment() 方法时，不仅要考虑对象实例的同步，还需要考虑类的级别锁。
+* 因此，同一时刻，只能有一个线程执行该静态同步方法。
+影响类的所有线程访问：多个线程可以并发执行不同对象的实例方法，但对于静态方法来说，
+* 多个线程即使操作不同的对象实例，也会在访问静态同步方法时竞争同一个类级别的锁。
+    *
+    * */
+
+
+
+    // 同步静态方法
+    // 同步静态方法
+    // 同步静态方法
+    // 同步静态方法
+    /*
+    * 如果方法是静态的，锁的是类的 Class 对象，而不是当前对象。这样可以确保多个线程在访问同一个类的静态方法时的同步。
+    * */
+    public synchronized static  void increment() {
+        counter++;
+    }
+    // 同步代码块
+    // 同步代码块
+    // 同步代码块
+    // 同步代码块
+
+    public void incrementone() {
+        // 只对共享资源部分加锁
+        synchronized (this) {
+            counter++;
+        }
+    }
+    public int getCounter() {
+        return counter;
+    }
+
+
+
+
+    private static final Object lock1 = new Object();  // 用于同步 counter1 的操作
+    private static final Object lock2 = new Object();  // 用于同步 counter2 的操作
+
+    private static int counter1 = 0;
+    private static int counter2 = 0;
+
+    public static void incrementCounter1() {
+        synchronized (lock1) {  // 使用 lock1 锁定代码块，确保线程安全
+            counter1++;
+        }
+    }
+
+    public static void incrementCounter2() {
+        synchronized (lock2) {  // 使用 lock2 锁定代码块，确保线程安全
+            counter2++;
+        }
+    }
+
+    public static int getCounter1() {
+        return counter1;
+    }
+
+    public static int getCounter2() {
+        return counter2;
+    }
+
+
+
+
+    @RequestMapping("/Synchronized")
+    public  void SynchronizedExample(String[] args) {
+
+        // 多个线程操作同一对象
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                increment();
+                System.out.println("thread1"+":"+counter);
+
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+               increment();
+                System.out.println("thread2"+":"+counter);
+            }
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        /*
+        * 加入了上面的代码 主线程会等待t1和t2执行完毕后 才会继续执行后面的代码
+        *
+        * join() 方法会使当前线程（主线程）等待调用它的线程（thread1 和 thread2）执行完毕后再继续执行。
+        * 也就是说，主线程会等待这两个线程完成后，才会输出最终的计数值 counter。
+         * */
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Counter Value: " + getCounter());
+
+/*
+*************************下面是演示synchronized (lock1) 和 synchronized (lock2)的格式
+* */
+
+        Thread thread1one = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                incrementCounter1();  // 操作 counter1
+            }
+        });
+
+        Thread thread2one = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+               incrementCounter2();  // 操作 counter2
+            }
+        });
+
+        thread1one.start();
+        thread2one.start();
+
+        try {
+            thread1one.join();  // 等待 thread1 完成
+            thread2one.join();  // 等待 thread2 完成
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Counter 1 Value: " + getCounter1());
+        System.out.println("Counter 2 Value: " + getCounter2());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
---
+++ douyin_service/douyin_test/src/main/java/com/bruce/testmain.java
@@ -0,0 +1,16 @@
+package com.bruce;
+
+public class testmain {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+
+}
