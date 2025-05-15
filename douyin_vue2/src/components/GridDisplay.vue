2f1b72d414d6043c10427224a5eb5e26184d8857
chenxmz <chenxmz@dcfcs.com>
2025-05-15 18:01:24 +0800

    增加了两个组件
    一个导航栏  一个父元素控制子元素的组件

---
+++ douyin_vue2/src/components/GridDisplay.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="grid-display"
+    :style="{
+      gap: gap + 'px',
+      gridTemplateColumns: `repeat(auto-fill, minmax(${itemWidth}px, 1fr))`,
+      gridAutoRows: itemHeight + 'px'
+    }"
+  >
+    <div v-for="(item, index) in items" :key="index" class="grid-item">
+      <img :src="item.icon" alt="" class="icon" />
+      <div class="icon-name">{{ item.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GridDisplay',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    itemWidth: {
+      type: Number,
+      required: true
+    },
+    itemHeight: {
+      type: Number,
+      required: true
+    },
+    gap: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.grid-display {
+  display: grid;
+  width: 100%;
+  box-sizing: border-box;
+  overflow-x: hidden; /* 防止横向滚动 */
+}
+
+.grid-item {
+  background: #eee;
+  display: flex;
+  flex-direction: column; /* 上下排列 */
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  padding: 8px 0;
+}
+
+.icon {
+  width: 40px;
+  height: 40px;
+  object-fit: contain;
+  margin-bottom: 6px;
+}
+
+.icon-name {
+  font-size: 14px;
+  color: #333;
+}
+</style>
---
+++ douyin_vue2/src/views/test/testshop.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="width: 100%;">
+    <GridDisplay
+      :itemWidth="80"
+      :itemHeight="100"
+      :items="items"
+      :gap="5"
+    />
+  </div>
+</template>
+
+<script>
+import GridDisplay from '@/components/GridDisplay.vue'
+
+export default {
+  components: { GridDisplay },
+  data() {
+    return {
+      items: [
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616408.png', name: '图标1' },
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616429.png', name: '图标2' },
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616490.png', name: '图标3' },
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616554.png', name: '图标4' },
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616494.png', name: '图标5' },
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616409.png', name: '图标6' },
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616426.png', name: '图标7' },
+        { icon: 'https://cdn-icons-png.flaticon.com/512/616/616453.png', name: '图标8' },
+      ]
+    }
+  }
+}
+</script>
